b116bfcb45a47371d1f1f8c99cbc0cfface7f2bf
rugek <rugek@dirtyhack.net>
2010-06-15 10:55:46 +0200

    removed trailing spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,27 @@
 # Copyright (C) 2009 Cognifide
-# 
+#
 # This file is part of Taskboard.
-# 
+#
 # Taskboard is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Taskboard is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Taskboard. If not, see <http://www.gnu.org/licenses/>.
 
 class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
-  
+
   attr_accessor :password_confirmation
   validates_confirmation_of :password
-  
+
   def validate
     errors.add_to_base("No password") if hashed_password.blank?
   end
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   def password
     @password
   end
-    
+
   def self.authenticate(username, password)
     user = self.find_by_username(username)
     if user
@@ -44,16 +44,16 @@ class User < ActiveRecord::Base
     end
     user
   end
-  
+
   private
 
   def create_new_salt
     self.salt = self.object_id.to_s + rand.to_s
   end
-    
+
   def self.encrypted_password(password, salt)
     string_to_hash = password + "taskboard".reverse + salt
     Digest::SHA1.hexdigest(string_to_hash)
   end
-  
+
 end
